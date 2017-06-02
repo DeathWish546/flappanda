9027b8725fcf956b9396280872d21a1a7bd05265
Daniel Shanker <daniel.shanker@indexexchange.com>
2017-06-02 11:49:08 -0400

    added assets

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+	public string SceneName = "flappanda";
+	// Use this for initialization
+	private bool clicked = false;
+	public void ChangeScene (string sceneName) {
+		SceneManager.LoadScene (sceneName);
+	}
+}
